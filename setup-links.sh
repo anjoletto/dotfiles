634a83b90e286e8db1e5d7b87e0b1cb2009e9044
Leonardo Anjoletto Ferreira <43471239+anjoletto@users.noreply.github.com>
2022-07-11 02:03:29 +0000

    fixed path for links

--- setup-links.sh
+++ setup-links.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env sh
 
+SPATH=$(dirname "$0")
+
 echo "Linking configuration files..."
-ln -s $(pwd)/zsh/zshenv $HOME/.zshenv
-ln -s $(pwd)/nvim $HOME/.config/nvim
-ln -s $(pwd)/tmux.conf $HOME/.config/tmux.conf
+ln -s $SPATH/zsh/zshenv $HOME/.zshenv
+ln -s $SPATH/nvim $HOME/.config/nvim
+ln -s $SPATH/tmux.conf $HOME/.config/tmux.conf
