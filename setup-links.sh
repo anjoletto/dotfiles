14e334d100718725ada12791ce955f72d8caadad
Leonardo Anjoletto Ferreira <43471239+anjoletto@users.noreply.github.com>
2022-07-19 22:05:42 -0300

    added bat config link

--- setup-links.sh
+++ setup-links.sh
@@ -3,4 +3,5 @@
 echo "Linking configuration files..."
 ln -s $(pwd)/zsh/zshenv $HOME/.zshenv
 ln -s $(pwd)/nvim $HOME/.config/nvim
+ln -s $(pwd)/bat $HOME/.config/bat
 ln -s $(pwd)/tmux.conf $HOME/.config/tmux.conf
