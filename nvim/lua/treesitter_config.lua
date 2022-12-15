46fe1b09a9021c98359f7cc496917459ad93b456
Leonardo Anjoletto Ferreira <43471239+anjoletto@users.noreply.github.com>
2022-12-15 13:47:13 -0300

    added json, yaml and markdown

--- nvim/lua/treesitter_config.lua
+++ nvim/lua/treesitter_config.lua
@@ -1,6 +1,6 @@
 require 'nvim-treesitter.install'.compilers = { "clang", "clang++"}
 require'nvim-treesitter.configs'.setup {
-  ensure_installed = { "nix", "lua", "python", "sql", "rust", "go" },
+  ensure_installed = { "nix", "lua", "python", "sql", "rust", "go", "yaml", "json", "markdown" },
   sync_install = false,
   auto_install = true,
   highlight = {
