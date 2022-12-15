985618398378935d9b31051663fa08caa702cf41
Leonardo Anjoletto Ferreira <43471239+anjoletto@users.noreply.github.com>
2022-12-15 13:47:30 -0300

    added go and rust

--- nvim/lua/lsp_config.lua
+++ nvim/lua/lsp_config.lua
@@ -44,6 +44,14 @@ require('lspconfig')['pyright'].setup{
 --     on_attach = on_attach,
 --     flags = lsp_flags,
 -- }
+require('lspconfig')['rust_analyzer'].setup{
+    on_attach = on_attach,
+    flags = lsp_flags,
+}
+require("lspconfig")["gopls"].setup{
+    on_attach = on_attach,
+    flags = lsp_flags,
+}
 -- require('lspconfig')['rust_analyzer'].setup{
 --     on_attach = on_attach,
 --     flags = lsp_flags,
