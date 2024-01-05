9fc648e09faf8fd103ade0fffc16a5fbef56a1ff
anjoletto <43471239+anjoletto@users.noreply.github.com>
2024-01-05 01:35:51 +0000

    feat: tmux configuration

--- setup.sh
+++ setup.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env sh
 
+echo "installing tmux and fuse..."j
 sudo apt update
 sudo apt install -y tmux fuse
 
+echo "install neovim..."j
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
 chmod u+x nvim.appimage
 mv nvim.appimage ~/.local/bin/nvim
+
+echo "linking confgiuration files..."
+cp tmux.conf ~/.tmux.conf
